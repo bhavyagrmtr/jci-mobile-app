49788937dc9d1a7c12a10a577f9399fb8afcca34
bhavyagrmtr <bhavy.agr.mtr@gmail.com>
2025-07-09 10:22:56 +0530

    Update layout and dependencies

--- jci-app/app/_layout.tsx
+++ jci-app/app/_layout.tsx
@@ -4,6 +4,7 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import { useColorScheme } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -17,33 +18,35 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          contentStyle: {
-            backgroundColor: colorScheme === 'dark' ? '#000' : '#fff',
-          },
-        }}
-      >
-        <Stack.Screen name="index" />
-        <Stack.Screen name="user-login" />
-        <Stack.Screen name="user-registration" />
-        <Stack.Screen name="admin-login" />
-        <Stack.Screen name="admin-app" />
-        <Stack.Screen name="user-app" />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-        <Stack.Screen 
-          name="ChangePasswordScreen" 
-          options={{ 
+    <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar backgroundColor="#000" style='dark' />
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack
+          screenOptions={{
             headerShown: false,
-            presentation: 'modal',
-            animation: 'slide_from_bottom'
-          }} 
-        />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+            contentStyle: {
+              backgroundColor: colorScheme === 'dark' ? '#000' : '#fff',
+            },
+          }}
+        >
+          <Stack.Screen name="index" />
+          <Stack.Screen name="user-login" />
+          <Stack.Screen name="user-registration" />
+          <Stack.Screen name="admin-login" />
+          <Stack.Screen name="admin-app" />
+          <Stack.Screen name="user-app" />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+          <Stack.Screen
+            name="ChangePasswordScreen"
+            options={{
+              headerShown: false,
+              presentation: 'modal',
+              animation: 'slide_from_bottom'
+            }}
+          />
+        </Stack>
+      </ThemeProvider>
+    </SafeAreaView>
   );
 }
